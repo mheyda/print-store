69caa20c0c30deaffd5ee1ab302593d4334f0e4b
Marshall <marshallheyda@gmail.com>
2022-02-14 21:31:15 -0500

    Fixed modal slideshow bug. Should be working correctly now

--- scripts.js
+++ scripts.js
@@ -23,18 +23,27 @@ for (var i = 0; i < listItems.length; i++) {
 
 // Select all product items
 document.querySelectorAll(".product-item").forEach(function(productItem, productIndex) {
+
+    var imageIndex = 0;
+    function updateImageIndex(newImageIndex) {
+        imageIndex = newImageIndex;
+    }
     
     // Function to change product image based on thumbnail that is clicked
-    productItem.querySelectorAll(".thumbnail").forEach(function(thumbnail, thumbIndex) {
+    productItem.querySelectorAll(".thumbnail").forEach(function(thumbnail, imageIndex) {
         // Listen for a click on the thumbnail image
+        console.log("Thumbnail imageIndex: " + imageIndex);
         thumbnail.onclick = function() {
+            console.log("Thumbnail imageIndex: " + imageIndex);
             // Call function to change the product image to the selected thumbnail
-            showThumb(productIndex, thumbIndex);
+            showThumb(productIndex, imageIndex);
+            updateImageIndex(imageIndex);
         }
     })
 
     // Function to open modal box for selected product
-    productItem.querySelectorAll(".product-image").forEach(function(productImage, imageIndex) {
+    productItem.querySelectorAll(".product-image").forEach(function(productImage) {
+        console.log("NEW IMAGEINDEX: " + imageIndex);
         // Listen for a click on the product image
         productImage.onclick = function() {
             // Open modal box
@@ -45,14 +54,6 @@ document.querySelectorAll(".product-item").forEach(function(productItem, product
             console.log("Open modal now has imageIndex: " + imageIndex);
         }
 
-        // Modal box switch to previous image
-        productItem.querySelector(".prev").onclick = function() {
-            console.log("Previous button received imageIndex: " + imageIndex);
-            imageIndex -= 1;
-            console.log("Previous button passing in imageIndex: " + imageIndex);
-            imageIndex = showSlides(productIndex, imageIndex);
-            console.log("Previous button now has imageIndex: " + imageIndex);
-        }
         // Modal box switch to next image
         productItem.querySelector(".next").onclick = function() {
             console.log("Next button received imageIndex: " + imageIndex);
@@ -61,52 +62,49 @@ document.querySelectorAll(".product-item").forEach(function(productItem, product
             imageIndex = showSlides(productIndex, imageIndex);
             console.log("Next button now has imageIndex: " + imageIndex);
         }
+
+        // Modal box switch to previous image
+        productItem.querySelector(".prev").onclick = function() {
+            console.log("Previous button received imageIndex: " + imageIndex);
+            imageIndex -= 1;
+            console.log("Previous button passing in imageIndex: " + imageIndex);
+            imageIndex = showSlides(productIndex, imageIndex);
+            console.log("Previous button now has imageIndex: " + imageIndex);
+        }
+        
+
+        // Close modal box for selected product
+        productItem.querySelector(".close").onclick = function() {
+            console.log("Closed button received imageIndex: " + imageIndex);
+            productItem.querySelector(".modal").style.display = "none";
+            showThumb(productIndex, imageIndex);
+            console.log("Closed button now has imageIndex: " + imageIndex);
+        }
     })
-
-    // Close modal box for selected product
-    productItem.querySelector(".close").onclick = function() {
-        productItem.querySelector(".modal").style.display = "none";
-    }
-
-    
 
 });
 
 
 
-function showThumb(productIndex, thumbIndex) {
+function showThumb(productIndex, imageIndex) {
     var item = document.querySelectorAll(".product-item")[productIndex];
     var slides = item.querySelectorAll(".product-image");
     for (var i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[thumbIndex].style.display = "block";
+    slides[imageIndex].style.display = "block";
 }
 
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
 function showSlides(productIndex, imageIndex) {
-    console.log(imageIndex);
     var item = document.querySelectorAll(".product-item")[productIndex];
     var slides = item.querySelectorAll(".mySlides");
-    
-    console.log("Slide: " + slides + "\nSlide length: " + slides.length);
-
-    if (imageIndex >= slides.length) {
-        console.log(imageIndex);
-        console.log("YES");
-        imageIndex = 0;
-        console.log(imageIndex);
-    }
+    if (imageIndex >= slides.length) {imageIndex = 0}
     if (imageIndex < 0) {imageIndex = slides.length - 1}
     for (var i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    console.log("Product Index: " + String(productIndex) + "\nImage Index: " + String(imageIndex));
     slides[imageIndex].style.display = "block";
-    console.log(slides[imageIndex]);
     return imageIndex;
 }
 

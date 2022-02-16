4260ad2ae47e8f0190ac9640839d608c7b1ce197
Marshall <marshallheyda@gmail.com>
2022-02-15 23:12:33 -0500

    Added size box and quantity box feature to auto update current value; Made improvements to design/CSS

--- scripts.js
+++ scripts.js
@@ -1,11 +1,4 @@
 
-// Update the price of a product when the user selects a size
-document.querySelectorAll('select').forEach(function(select, i) {
-    select.onchange = function() {
-        var price = select.options[select.selectedIndex].dataset.price;
-        document.querySelectorAll('.price')[i].innerHTML = `$${price}`;
-    }
-});
 
 // Initialize the product images
 var listItems = document.querySelectorAll(".product-item");
@@ -82,6 +75,52 @@ function showSlides(productIndex, imageIndex) {
     slides[imageIndex].style.display = "block";
     return imageIndex;
 }
+
+
+
+
+
+// Initialize previously selected size value
+var previousSizeIndex = 0;
+var previousSizeHTML = "Select Size";
+// Select size box for all product items
+document.querySelectorAll('.size').forEach(function(sizeSelect, i) {
+    
+    sizeSelect.onchange = function() {
+        // Update the size box to show `Size: ${selected size}` and replace previously selected size with original value
+        sizeSelect.querySelectorAll("option")[previousSizeIndex].innerHTML = previousSizeHTML;
+        previousSizeIndex = sizeSelect.selectedIndex;
+        previousSizeHTML = sizeSelect.querySelectorAll("option")[previousSizeIndex].innerHTML;
+        var selectedOption = sizeSelect.querySelectorAll("option")[sizeSelect.selectedIndex];
+        selectedOption.innerHTML = `Size: ${selectedOption.innerHTML}`;
+
+        // Update the price of a product when the user selects a size
+        var price = sizeSelect.options[sizeSelect.selectedIndex].dataset.price;
+        document.querySelectorAll('.price')[i].innerHTML = `$${price}`;
+    }
+});
+
+
+
+// Initialize previously selected quantity value
+var previousQtyIndex = 0;
+// Select quantity box for all product items
+document.querySelectorAll(".quantity").forEach(function(quantitySelect) {
+    // Create options for every quantity select box
+    for (var i = 1; i < 31; i++) {
+        var option = document.createElement('option');
+        option.value = i;
+        option.innerHTML = i;
+        quantitySelect.appendChild(option);
+    }
+    // If quantity is changed, update quantity box to show `Quantity: ${selected amount}` and replace previously selected quantity with original value
+    quantitySelect.onchange = function() {
+        quantitySelect.querySelectorAll("option")[previousQtyIndex].innerHTML = previousQtyIndex;
+        quantitySelect.querySelectorAll("option")[quantitySelect.selectedIndex].innerHTML = `Quantity: ${quantitySelect.selectedIndex}`;
+        previousQtyIndex = quantitySelect.selectedIndex;
+    }
+});
+
 
 
 
